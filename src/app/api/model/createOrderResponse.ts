188a24093ac8604b5f16f178bff9fadc1d72ffd4
awerdj93 <awerdj93@hotmail.com>
2020-04-18 13:34:30 +0800

    Added remember me

--- src/app/api/model/createOrderResponse.ts
+++
@@ -1,3 +0,0 @@
-export class CreateOrderResponse {
-    id?: number;
-}
--- src/app/components/login-panel/login-panel.component.ts
+++ src/app/components/login-panel/login-panel.component.ts
@@ -15,6 +15,7 @@ export class LoginPanelComponent implements OnInit {
   form: FormGroup;
   formState: FormState;
   error: string;
+  rmbMe: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -31,8 +32,9 @@ export class LoginPanelComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      username: [null, Validators.required],
-      password: [null, Validators.required]
+      email: [localStorage.getItem('email'), Validators.required],
+      password: [localStorage.getItem('password'), Validators.required],
+      remember: [localStorage.getItem('remember')]
     });
     this.formState = new FormState(this.form);
     // get return url from route parameters or default to '/'
@@ -42,9 +44,17 @@ export class LoginPanelComponent implements OnInit {
   onSubmit() {
     this.error = null;
     if (this.formState.valid) {
-      this.authenticationService.token(this.form.controls.username.value, this.form.controls.password.value)
+      this.authenticationService.token(this.form.controls.email.value, this.form.controls.password.value)
         .subscribe(
           token => {
+            if (this.form.controls.remember.value) {
+              localStorage.setItem('email', this.form.controls.email.value);
+              localStorage.setItem('password', this.form.controls.password.value); 
+            } else {
+              localStorage.removeItem('email');
+              localStorage.removeItem('password'); 
+            }
+            localStorage.setItem('remember', this.form.controls.remember.value);   
             this.authenticationService.login(token.authorization).subscribe(
               user => {
                 this.router.navigate(['/profile']);
